eba3d3e295d259b0f2c4c4ac648743f786c798ca
Talthunayan <78157264+Talthunayan@users.noreply.github.com>
2021-04-05 00:23:10 +0300

    Start up page Done

--- components/StartUpPage.js
+++ components/StartUpPage.js
@@ -1,63 +1,76 @@
 import React from "react";
 import styled from "styled-components/native";
+import ImageBackground from "react-native";
+import BackgroundIMG from "../assets/BackgroundIMG6.gif";
 
-export const HomeBackground = styled.View`
-  background-color: blue;
+//Styling
+
+export const HomeBackground = styled.ImageBackground`
   flex: 1;
   width: 100%;
   height: 100%;
+    justify-content:center;
 `;
-
 export const TopStyling = styled.View`
   height: 40%;
+  
+  background-color:rgba(221, 190, 169, 0.6);
   align-items: center;
-  justify-content: center;
+  /* justify-content: center; */
 `;
 
 export const Title = styled.Text`
-  /* color: rgb(84, 68, 79); */
-  color: rgb(252, 236, 247);
-  font-weight: bold;
-  font-style: italic;
-  /* font-family:  */
-  font-size: 43;
-  text-align: center;
+padding: 15%;
+  color: #f5f1ed;
+  font-size: 35;
+  text-align: left;
+`;
+
+export const Sentence = styled.Text`
+
+padding: 1%;
+  color: #f5f1ed;
+  font-size: 20;
+  /* text-align: center; */
 `;
 
 export const OverLayContainer = styled.View`
   flex: 1;
-  background-color: rgba(140, 114, 131, 0.25);
+  background-color: #fff1e6;
 `;
 
 export const BottomStyling = styled.View`
   height: 40%;
-  align-items: center;
-  justify-content: center;
+flex-direction:row;
+  align-items:center;
+  justify-content:space-between;
 `;
 
 export const ButtonStyled = styled.Text`
   font-size: 15;
   color: white;
   font-weight: bold;
-  width: 220px;
+  
+  width: 200px;
   text-align: center;
-  margin-bottom: 10px;
-  padding: 30px;
-  border-width: 5px;
-  background-color: rgb(84, 68, 79);
-  border-color: white;
-  border-radius: 20px;
+  padding: 5%;
+  background-color:rgba(221, 190, 169, 0.6);
 `;
+
+//**************** Function *************
 
 const StartUp = ({ navigation }) => {
   return (
-    <HomeBackground>
+
       <OverLayContainer>
+          <HomeBackground source={BackgroundIMG}>
         <TopStyling>
-          <Title>Hello!</Title>
+     
+         
         </TopStyling>
+        <Title>Get Started ...</Title>
+          <Sentence>have the ability to connect with friends and family ...</Sentence>
         <BottomStyling>
-          {/* <ButtonStyled onPress={() => navigation.navigate("FlowersList")}> */}
           <ButtonStyled onPress={() => navigation.navigate("SignIn")}>
             Sign In
           </ButtonStyled>
@@ -68,8 +81,9 @@ const StartUp = ({ navigation }) => {
             Or Here
           </ButtonStyled> */}
         </BottomStyling>
+        </HomeBackground>
       </OverLayContainer>
-    </HomeBackground>
+   
   );
 };
 
